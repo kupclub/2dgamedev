2ceb50c0606d0c1b47cbab4aa087642f25f11107
Colby Eckert <colbyeckert@outlook.com>
2018-02-27 19:26:42 -0500

    maps

--- map.lua
+++ map.lua
@@ -3,14 +3,20 @@
 
 local map = {}
 
-function map.load() 
+function map:load() 
+
+	map.maps = {
+		test = sti("res/test.lua")
+	}
 
 end
 
-function map.update(dt)
-
+function map:update(dt)
+	map.maps.test:update(dt)
 end
 
-function map.draw()
-
+function map:draw()
+	map.maps.test:draw()
 end
+
+return map
