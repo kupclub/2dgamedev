a44c99235afba73ce91ac6240e0bfca69ba0f689
ncharlie <charles.saternos@gmail.com>
2018-04-10 17:50:06 -0400

    proper player collision

--- net.lua
+++ net.lua
@@ -6,33 +6,34 @@ UPDATERATE = 0.1
 local t = {}
 
 function t.connect(addr, port)
-  local udp = socket.udp()
-  udp:settimeout(0)
-  udp:setpeername(address, port)
-  return udp
+    local udp = socket.udp()
+    udp:settimeout(0)
+    udp:setpeername(address, port)
+    return udp
 end
 
 function t.pokeServer(connection, state)
-  connection:send(bitser.dumps(state))
+    connection:send(bitser.dumps(state))
 end
 
 function t.runServer()
-  local udp = socket.udp()
+    local udp = socket.udp()
 
-  udp:settimeout(0)
-  udp:setsockname('*', 1234)
+    udp:settimeout(0)
+    udp:setsockname('*', 1234)
 
-  while true do
-    local data, msg_or_ip, port_or_nil = udp:receivefrom()
-    if data then
-      local b = bitser.loads(data)
-      if type(b) == "table" then
-        state = b
-      else
-        print("unexpected type of data", b)
-      end
+    while true do
+        local data, msg_or_ip, port_or_nil = udp:receivefrom()
+        if data then
+            local b = bitser.loads(data)
+            if type(b) == "table" then
+                state = b
+            else
+                print("unexpected type of data", b)
+            end
+        end
+        sleep(0.01)
     end
-  end
 end
 
 return t
